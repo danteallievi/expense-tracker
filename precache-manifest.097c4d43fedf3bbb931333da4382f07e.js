b5921b9d9546cb1e58a58d9a8a04ecd0f09e10cd
Dante Allievi <toto99.-@hotmail.com>
2020-10-27 12:30:09 +0100

    Updates

---
+++ precache-manifest.097c4d43fedf3bbb931333da4382f07e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f53fb8ca57ed5d3be07d04264cb4f2",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "6afa6a546c3f638cd60a",
+    "url": "/expense-tracker/static/css/main.caf09185.chunk.css"
+  },
+  {
+    "revision": "3e036a7e31c5483c7846",
+    "url": "/expense-tracker/static/js/2.44dbab4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.44dbab4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6afa6a546c3f638cd60a",
+    "url": "/expense-tracker/static/js/main.8d0831e9.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
